b802f544768d97cd38e2b1a74f31a6d51b3c44c6
Ghost1227 <dgriffiths@section214.com>
2017-02-14 16:53:01 -0600

    Add copy link button for Chris

--- webview.js
+++ webview.js
@@ -1,8 +1,63 @@
+const path = require('path');
+
 module.exports = (Franz, options) => {
+	Franz.injectCSS(path.join(__dirname, 'css', 'franz.css'));
+
+	function copyToClipboard(elem) {
+		let targetId = '_hiddenURLField';
+		let origSelectionStart, origSelectionEnd;
+		let target = document.getElementById(targetId);
+
+		if(!target) {
+			target = document.createElement('textarea');
+			target.style.position = 'absolute';
+			target.style.left = '-9999px';
+			target.style.top = '0';
+			target.id = targetId;
+			document.body.appendChild(target);
+		}
+
+		target.textContent = window.location.href;
+
+		let currentFocus = document.activeElement;
+
+		target.focus();
+		target.setSelectionRange(0, target.value.length);
+
+		document.execCommand('copy');
+
+		if(currentFocus && typeof currentFocus.focus === 'function') {
+			currentFocus.focus();
+		}
+	}
+
 	function getMessages() {
 		let mine = '';
 		let unassigned = '';
 		let total = '0';
+
+		if ($('.convo-toolbar').length) {
+			if (! $('#copyLink').length) {
+				$('#actions-dd .more').append('<li class="actions-dd"><a id="copyLink" class="actions-dd">Copy Link</a></li>');
+				$('.c-convo-toolbar').after('<div class="link-copied" style="display: none">Ticket URL copied to clipboard!<a id="closeLink">x</a></div>');
+			}
+		}
+
+		$('#copyLink').on('click', function(e) {
+			e.preventDefault();
+
+			copyToClipboard();
+
+			$('.link-copied').fadeIn('fast').css('display', 'block');
+		});
+
+		$('#closeLink').on('click', function(e) {
+			e.preventDefault();
+
+			$('.link-copied').fadeOut('fast', function () {
+				$(this).css('display', 'none');
+			});
+		})
 
 		if ($('.dropdown.mailboxes').length && $('.dropdown.mailboxes a').hasClass('active')) {
 			mine = $('li.mine a .badge').text();
@@ -28,11 +83,11 @@ module.exports = (Franz, options) => {
 			unassigned = unassigned.toString();
 		}
 
-		if (mine != '') {
+		if (mine !== '') {
 			total = mine;
 		}
 
-		if (unassigned != '') {
+		if (unassigned !== '') {
 			total = total + '/' + unassigned;
 		}
 
@@ -40,4 +95,4 @@ module.exports = (Franz, options) => {
 	}
 
 	Franz.loop(getMessages);
-}
+};
